e4f183f1db6f56d3d597faf05841c95b74466566
AsrafulMasum <h.m.asrafulmasum@gmail.com>
2023-11-06 13:15:20 +0600

    assignment post and get api created

--- index.js
+++ index.js
@@ -28,6 +28,21 @@ const client = new MongoClient(uri, {
   }
 });
 
+// jwt middleware
+const verifyCookie = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized Access" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized Access" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
 const dbConnect = async () => {
   try {
     client.connect();
@@ -38,9 +53,54 @@ const dbConnect = async () => {
 };
 dbConnect();
 
+const database = client.db("eduCareDB");
+const usersCollections = database.collection("usersDB");
+const assignmentsCollections = database.collection("assignmentsDB");
+
+// jwt api method
+app.post("/jwt", (req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "4h",
+  });
+  res
+    .cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    })
+    .send({ success: true });
+});
+
 app.get("/", (req, res) => {
   res.send("server is running data will be appear soon...");
 });
+
+// users api method
+app.get("/users", async (req, res) => {
+  const cursor = usersCollections.find();
+  const result = await cursor.toArray();
+  res.send(result);
+});
+
+app.post("/users", async (req, res) => {
+  const user = req.body;
+  const result = await usersCollections.insertOne(user);
+  res.send(result);
+});
+
+// assignment api method
+
+app.get("/assignments", async (req, res) => {
+  const result = await assignmentsCollections.find().toArray()
+  res.send(result)
+})
+
+app.post("/assignments", async(req, res) => {
+  const assignmentInfo = req.body
+  const result = await assignmentsCollections.insertOne(assignmentInfo)
+  res.send(result)
+})
 
 app.listen(port, () => {
   console.log(`server is running on port: ${port}`);
